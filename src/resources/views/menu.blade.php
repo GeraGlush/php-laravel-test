a53154802046b10b838440380f58d09662668729
GeraGlush <75035157+GeraGlush@users.noreply.github.com>
2023-10-05 18:52:22 +0300

    Create menu.blade.php

---
+++ src/resources/views/menu.blade.php
@@ -0,0 +1,10 @@
+@extends('includes/layout')
+
+@section('title', 'Главная страница')
+
+@section('content')
+    <div class="text-center">
+        <h1>Добро пожаловать на главную страницу</h1>
+        <p>Здесь будет ваш контент для главной страницы.</p>
+    </div>
+@endsection
