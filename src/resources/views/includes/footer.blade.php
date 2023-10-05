1f9fdd5b61dc52f5cdb3bd62902985ac365f62cc
GeraGlush <75035157+GeraGlush@users.noreply.github.com>
2023-10-05 18:52:27 +0300

    Create footer.blade.php

---
+++ src/resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+@section('footer')
+    <footer class="my-5 pt-5 text-body-secondary text-center text-small default_cursor_cs">
+        <p class="default_cursor_cs">© 2023 Компания</p>
+            <a href="https://api.cargotech.pro/web/admin/main">Api manager</a></li>
+    </footer>
