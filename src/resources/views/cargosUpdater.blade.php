0dba7c2530d31e7a02f3a0f5f3aea0c4e220b834
GeraGlush <75035157+GeraGlush@users.noreply.github.com>
2023-10-05 18:52:33 +0300

    Create cargosUpdater.blade.php

---
+++ src/resources/views/cargosUpdater.blade.php
@@ -0,0 +1,8 @@
+@extends('includes.layout')
+
+@section('title', 'Обновление грузов')
+
+@section('content')
+    <h1>Добро пожаловать на главную страницу</h1>
+    <p>Здесь будет ваш контент для главной страницы.</p>
+@endsection
