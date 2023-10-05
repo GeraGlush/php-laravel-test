a1a6ab59f4478867788ed210d53ea5cd6279eb75
GeraGlush <75035157+GeraGlush@users.noreply.github.com>
2023-10-05 18:52:29 +0300

    Create getCargosApi.blade.php

---
+++ src/resources/views/getCargosApi.blade.php
@@ -0,0 +1,8 @@
+@extends('includes/layout')
+
+@section('title', 'Получить грузы по API')
+
+@section('content')
+    <h1>Добро пожаловать на главную страницу</h1>
+    <p>Выберите действие с API</p>
+@endsection
