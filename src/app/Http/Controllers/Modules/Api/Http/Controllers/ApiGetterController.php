207752a31ba82e17b0d7854b79ae4c18f993dbc5
GeraGlush <75035157+GeraGlush@users.noreply.github.com>
2023-10-23 20:16:45 +0300

    Create ApiGetterController.php

---
+++ src/app/Http/Controllers/Modules/Api/Http/Controllers/ApiGetterController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Modules\Api\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiGetterController extends Controller
+{
+    //
+}
